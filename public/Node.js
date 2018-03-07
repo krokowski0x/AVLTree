2e3fb46be8c9b60fc05b0c5df1aa49640b7d0226
krokowski0x <krokowski.dx@gmail.com>
2018-03-07 22:27:27 +0100

    Initial commit

---
+++ public/Node.js
@@ -0,0 +1,20 @@
+class Node {
+
+  constructor(key, value) {
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+    this.height = 0;
+    this.balanceFactor = 0;
+    this.key = key;
+    this.value = value;
+  }
+
+  rotateRight() {
+
+  }
+
+  rotateLeft() {
+
+  }
+}
---
+++ public/Tree.js
@@ -0,0 +1,43 @@
+class Tree {
+
+  constructor() {
+    this.root = null;
+    this.height = 0;
+  }
+
+  compare(a, b) {
+    
+  }
+
+  get root() {
+
+  }
+
+  get treeHeight() {
+
+  }
+
+  insertNode(key, data) {
+
+  }
+
+  removeNode(key) {
+
+  }
+
+  find(key) {
+
+  }
+
+  preOrder() {
+
+  }
+
+  inOrder() {
+
+  }
+
+  postOrder() {
+
+  }
+}
