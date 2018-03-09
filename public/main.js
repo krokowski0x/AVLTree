410587efffce8237adff455416558b883ff9928a
krokowski0x <krokowski.dx@gmail.com>
2018-03-09 22:01:40 +0100

    Added insert and visualisation tests

--- public/main.js
+++ public/main.js
@@ -1,7 +1,13 @@
 function setup() {
-  createCanvas(400,400);
+  createCanvas(800,800);
+  background(51);
+  tree = new Tree();
+  for (let i = 0; i < 15; i++) {
+    tree.insertNode(floor(random(0, 100)), 'node');
+  }
+  tree.preOrder();
 }
 
 function draw() {
-  background(51);
+
 }
