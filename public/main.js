54fa648b50812472caacf75620752227e2051cf6
krokowski0x <krokowski.dx@gmail.com>
2018-03-10 10:26:41 +0100

    Added basic user interface

--- public/main.js
+++ public/main.js
@@ -1,13 +1,28 @@
 function setup() {
-  createCanvas(800,800);
+  // Width enough for 5 levels (32 leaves)
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
   background(51);
+
+  // Fill tree with random nodes
   tree = new Tree();
   for (let i = 0; i < 15; i++) {
     tree.insertNode(floor(random(0, 100)), 'node');
   }
-  tree.preOrder();
 }
 
 function draw() {
 
 }
+
+document.getElementsByClassName('btn')[0]
+  .addEventListener('click', function(e) {
+
+    let key = document.getElementsByClassName('key')[0].value;
+    console.log(key);
+    let val = document.getElementsByClassName('value')[0].value;
+    console.log(val);
+    tree.insertNode(key, val);
+    e.preventDefault();
+  });
