aa0159f0580fb49a854dbaa1e0f6bf83e6e76cdb
krokowski0x <krokowski.dx@gmail.com>
2018-03-14 20:15:21 +0100

    Added remove, find and toggle switch event listeners

--- public/main.js
+++ public/main.js
@@ -1,3 +1,6 @@
+let previousKey = false;
+let toggleValues = false;
+
 function setup() {
   // Width enough for 5 levels (32 leaves)
   canvas = createCanvas(windowWidth, windowHeight);
@@ -26,16 +29,63 @@ document.getElementsByClassName('insertBtn')[0]
     let val = document.getElementsByClassName('value')[0].value;
     tree.insertNode(key, val);
 
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode(key);
-    node.appendChild(textnode);
-    document.getElementsByTagName("FORM")[0].appendChild(node);
+    document.getElementsByClassName('key1')[0].value = '';
+    document.getElementsByClassName('value')[0].value = '';
   });
 
-  document.getElementsByClassName('removeBtn')[0]
-    .addEventListener('click', function(e) {
-      e.preventDefault();
+document.getElementsByClassName('removeBtn')[0]
+  .addEventListener('click', function(e) {
+    e.preventDefault();
 
-      let key = parseInt(document.getElementsByClassName('key2')[0].value);
-      tree.removeNode(key);
-    });
+    let key = parseInt(document.getElementsByClassName('key2')[0].value);
+    tree.removeNode(key);
+
+    document.getElementsByClassName('key2')[0].value = '';
+  });
+
+document.getElementsByClassName('findBtn')[0]
+  .addEventListener('click', function(e) {
+    e.preventDefault();
+
+    if (previousKey) {
+      clear();
+      background(51);
+      tree.preOrder();
+    }
+
+    let key = parseInt(document.getElementsByClassName('key3')[0].value);
+    let node = tree.find(key);
+    strokeWeight(3);
+    stroke('green');
+    noFill();
+    ellipse(node.x, node.y, 40, 40);
+
+    document.getElementsByClassName('key3')[0].value = '';
+    previousKey = true;
+  });
+
+document.getElementsByClassName('onoffswitch-label')[0]
+  .addEventListener('click', function(e) {
+    toggleValues = !toggleValues;
+    clear();
+    background(51);
+    tree.preOrder(toggleValues);
+  });
+
+function updateInfo() {
+  let info = document.getElementsByClassName('tree-data')[0];
+  info.innerHTML = `
+    <tr>
+      <th>Root:</th>
+      <th>${tree.root.key} // ${tree.root.value}</th>
+    </tr>
+    <tr>
+      <th>Tree height:</th>
+      <th>${tree.height}</th>
+    </tr>
+    <tr>
+      <th>Number of nodes:</th>
+      <th>${tree.nodes}</th>
+    </tr>
+  `;
+}
